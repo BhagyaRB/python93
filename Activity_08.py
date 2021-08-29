8cd6a547debcce285b6a7f37c6d6e0fea1ae5ab3
BhagyaRB <89718535+BhagyaRB@users.noreply.github.com>
2021-08-29 22:17:34 +0530

    Update Activity_08.py
    an interpreted language is a programming language whose implementations execute instructions directly and freely.  
    
    2)Difference between compiled and  interpreted languages
      A compiled language is a programming language whose implementations are typically compilers and not interpreters.
    An interpreted language is a programming language whose implementations execute instructions directly and freely, without previously compiling a program into machine-language instructions.
    
    
    
    
    
    
    
    
    2
    There are at least two steps to get from source code to execution.
    There is only one steps to get from source code to execution.
    3
    In this language, compiled programs run faster than interpreted programs.
    While in this language, interpreted programs can be modified while the program is running.
    4
    
    
    In this language, compilation errors prevent the code from compiling.
    In this languages, all the debugging occurs at run-time.
    5
    The code of compiled language can be executed directly by the computer’s CPU.
    A program written in an interpreted language is not compiled, it is interpreted.
    6
    This language delivers better performance.
    This languages delivers relatively slower performance.
    7
    Example of compiled language – C, C++, C#, CLEO, COBOL, etc.
    Example of Interpreted language – JavaScript, Perl, Python, BASIC, etc.
    
    3)       c    language

---
+++ Activity_08.py
@@ -0,0 +1,35 @@
+an interpreted language is a programming language whose implementations execute instructions directly and freely.  
+
+2)Difference between compiled and  interpreted languages
+  A compiled language is a programming language whose implementations are typically compilers and not interpreters.
+An interpreted language is a programming language whose implementations execute instructions directly and freely, without previously compiling a program into machine-language instructions.
+
+
+
+
+
+
+
+
+2
+There are at least two steps to get from source code to execution.
+There is only one steps to get from source code to execution.
+3
+In this language, compiled programs run faster than interpreted programs.
+While in this language, interpreted programs can be modified while the program is running.
+4
+
+
+In this language, compilation errors prevent the code from compiling.
+In this languages, all the debugging occurs at run-time.
+5
+The code of compiled language can be executed directly by the computer’s CPU.
+A program written in an interpreted language is not compiled, it is interpreted.
+6
+This language delivers better performance.
+This languages delivers relatively slower performance.
+7
+Example of compiled language – C, C++, C#, CLEO, COBOL, etc.
+Example of Interpreted language – JavaScript, Perl, Python, BASIC, etc.
+
+3)       c    language
