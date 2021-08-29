7e72cdac9fc522a8386b47b3d5ea57966d59f862
BhagyaRB <89718535+BhagyaRB@users.noreply.github.com>
2021-08-29 16:40:42 +0530

    Update Activity_05.py
    Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
    Type "help", "copyright", "credits" or "license()" for more information.
    >>> x=list(map(int,input().strip().split()))
    
    >>> x=[2,4,9,1,5]
    >>> [2,4,9,1,5]
    [2, 4, 9, 1, 5]
    >>> Addition=sum(x)
    >>> print(Addition)
    21
    >>>

---
+++ Activity_05.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=list(map(int,input().strip().split()))
+
+>>> x=[2,4,9,1,5]
+>>> [2,4,9,1,5]
+[2, 4, 9, 1, 5]
+>>> Addition=sum(x)
+>>> print(Addition)
+21
+>>> 
