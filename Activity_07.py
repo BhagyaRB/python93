b5a31e598250ac2bee05d2acbdee4524893bf036
BhagyaRB <89718535+BhagyaRB@users.noreply.github.com>
2021-08-29 21:44:32 +0530

    Update Activity_07.py
    Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
    Type "help", "copyright", "credits" or "license()" for more information.
    >>> a=int(input("enter first number:"))
    enter first number:2
    >>> b=int(input("enter second number:"))
    enter second number:3
    >>> sum=a+b
    >>> print("sum:",sum)
    sum: 5
    >>>

---
+++ Activity_07.py
@@ -0,0 +1,10 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter first number:"))
+enter first number:2
+>>> b=int(input("enter second number:"))
+enter second number:3
+>>> sum=a+b
+>>> print("sum:",sum)
+sum: 5
+>>> 
