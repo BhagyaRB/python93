18169cca4e26c45deab7c985a552715faeefcf4f
BhagyaRB <89718535+BhagyaRB@users.noreply.github.com>
2021-08-29 23:17:48 +0530

    Update Activity_10.py
    Completely True
    Completely False
    Partially False, Has a compiler but not the most advanced
     
    Option 4
    Correct answer
    Completely False
     
    Which of the following is not a valid print statement
    0/1
    print("%d + %d = %d"%(a, b, a+b))
    print("{1} + {2} = {0}".format(a+b, a, b))
    print(a, " + ", b, " = ", a+b)
     
    print("{2} + {0} = {1}".format(a+b, a, b))
    Correct answer
    print("{2} + {0} = {1}".format(a+b, a, b))
     
    The output of the following snippet
    1/1
    Captionless Image
    hiiiihiiiiiihiiiiiihiiiihiiiihiii
     
    hiiiihiiiiiihiiiiiihiiiihiiiihiiihiii
    iiihiiiihiiiiiihiiiiiihiiiihiiiihiii
    hihiiiihiiiiiihiiiiiihiiiihiiiihiii
     
    Slice operation can be performed on
    0/1
    Lists
     
    Strings
    Dictonaries
    Tuples
    Correct answer
    Lists
    Strings
    Tuples
     
    What is the output of the following code snippet.
    0/1
    Captionless Image
    
    Option 1
     
    
    Option 2
    
    Option 3
    None of these
    Correct answer
    Option 2
     
    Lists in python are mutable
    1/1
    True
     
    False
     
    Tuples in python are immutable
    1/1
    True
     
    False
     
    Python is an object oriented programming language. *
    1/1
    True
     
    False
    This form was created inside of BMS College of Engineering.
    Google Forms

---
+++ Activity_10.py
@@ -0,0 +1,143 @@
+Completely True
+Completely False
+Partially False, Has a compiler but not the most advanced
+ 
+Option 4
+Correct answer
+Completely False
+ 
+Which of the following is not a valid print statement
+0/1
+print("%d + %d = %d"%(a, b, a+b))
+print("{1} + {2} = {0}".format(a+b, a, b))
+print(a, " + ", b, " = ", a+b)
+ 
+print("{2} + {0} = {1}".format(a+b, a, b))
+Correct answer
+print("{2} + {0} = {1}".format(a+b, a, b))
+ 
+The output of the following snippet
+1/1
+Captionless Image
+hiiiihiiiiiihiiiiiihiiiihiiiihiii
+ 
+hiiiihiiiiiihiiiiiihiiiihiiiihiiihiii
+iiihiiiihiiiiiihiiiiiihiiiihiiiihiii
+hihiiiihiiiiiihiiiiiihiiiihiiiihiii
+ 
+Slice operation can be performed on
+0/1
+Lists
+ 
+Strings
+Dictonaries
+Tuples
+Correct answer
+Lists
+Strings
+Tuples
+ 
+What is the output of the following code snippet.
+0/1
+Captionless Image
+
+Option 1
+ 
+
+Option 2
+
+Option 3
+None of these
+Correct answer
+Option 2
+ 
+Lists in python are mutable
+1/1
+True
+ 
+False
+ 
+Tuples in python are immutable
+1/1
+True
+ 
+False
+ 
+Python is an object oriented programming language. *
+1/1
+True
+ 
+False
+This form was created inside of BMS College of Engineering.
+Google FormsCompletely True
+Completely False
+Partially False, Has a compiler but not the most advanced
+ 
+Option 4
+Correct answer
+Completely False
+ 
+Which of the following is not a valid print statement
+0/1
+print("%d + %d = %d"%(a, b, a+b))
+print("{1} + {2} = {0}".format(a+b, a, b))
+print(a, " + ", b, " = ", a+b)
+ 
+print("{2} + {0} = {1}".format(a+b, a, b))
+Correct answer
+print("{2} + {0} = {1}".format(a+b, a, b))
+ 
+The output of the following snippet
+1/1
+Captionless Image
+hiiiihiiiiiihiiiiiihiiiihiiiihiii
+ 
+hiiiihiiiiiihiiiiiihiiiihiiiihiiihiii
+iiihiiiihiiiiiihiiiiiihiiiihiiiihiii
+hihiiiihiiiiiihiiiiiihiiiihiiiihiii
+ 
+Slice operation can be performed on
+0/1
+Lists
+ 
+Strings
+Dictonaries
+Tuples
+Correct answer
+Lists
+Strings
+Tuples
+ 
+What is the output of the following code snippet.
+0/1
+Captionless Image
+
+Option 1
+ 
+
+Option 2
+
+Option 3
+None of these
+Correct answer
+Option 2
+ 
+Lists in python are mutable
+1/1
+True
+ 
+False
+ 
+Tuples in python are immutable
+1/1
+True
+ 
+False
+ 
+Python is an object oriented programming language. *
+1/1
+True
+ 
+False
+This form was created inside of BMS College of Engineering.
+Google Forms
