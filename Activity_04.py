d406d95a988e23aef14ea397a74b57cd7aadd4ae
BhagyaRB <89718535+BhagyaRB@users.noreply.github.com>
2021-08-29 12:35:30 +0530

    Update Activity_04.py
    Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
    Type "help", "copyright", "credits" or "license()" for more information.
    >>> n=int(input("enter the value for n="))
    enter the value for n=4
    >>> m=int(input("enter the value for m="))
    enter the value for m=8
    >>> p=n+m
    >>> print(P)
    Traceback (most recent call last):
      File "<pyshell#3>", line 1, in <module>
        print(P)
    NameError: name 'P' is not defined
    >>> print(p)
    12
    >>>

---
+++ Activity_04.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=int(input("enter the value for n="))
+enter the value for n=4
+>>> m=int(input("enter the value for m="))
+enter the value for m=8
+>>> p=n+m
+>>> print(P)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    print(P)
+NameError: name 'P' is not defined
+>>> print(p)
+12
+>>> 
